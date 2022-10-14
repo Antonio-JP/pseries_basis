3ecf530ca58d59043dfa73f3843871ae6e3122c8
Antonio Jimenez-Pastor <kethewal@gmail.com>
2022-10-14 10:35:50 +0200

    Sequences as Maps  - Now Sequences inherit from SetMorphism. All sequences are maps between NN and some universe.  - In LambdaSequence: we can create these sequences allowing generic inputs (see flag allow_sym). If the output is not in the universe but is symbolid, we return the corresponding expression.  - Added methodd __eq__ and __hash__ to LambdaSequence  - Added method "almost_equal" to Sequence.

--- pseries_basis/sequences.py
+++ pseries_basis/sequences.py
@@ -8,20 +8,27 @@ r'''
     This module will create a framework for the whole package to manage, access and manipulate sequences
     in a generic fashion.
 '''
-from abc import ABCMeta, abstractmethod
+## Python imports
 try: # python 3.9 or higher
     from functools import cache
 except ImportError: #python 3.8 or lower
     from functools import lru_cache as cache
 
+## Sage imports
+from sage.categories.homset import Hom
 from sage.categories.pushout import CoercionException, pushout
+from sage.categories.morphism import SetMorphism
+from sage.categories.sets_cat import Sets
+from sage.rings.semirings.non_negative_integer_semiring import NN
+from sage.symbolic.ring import SR
 
-class Sequence(metaclass=ABCMeta):
+class Sequence(SetMorphism):
     r'''
         Main calss for sequences. It defines a universe for its elements and a general interface to access the sequence.
     '''
     def __init__(self, universe):
         self.__universe = universe
+        super().__init__(Hom(NN,universe,Sets()), lambda n : self.element(n))
 
     @property
     def universe(self): 
@@ -49,9 +56,8 @@ class Sequence(metaclass=ABCMeta):
     def __call__(self, input):
         return self.element(input)
 
-    @abstractmethod
     def element(self, index : int):
-        pass
+        raise NotImplementedError("Method 'element' not implemented")
 
     def almost_zero(self, order=10):
         r'''
@@ -62,6 +68,43 @@ class Sequence(metaclass=ABCMeta):
             sequence is exactly zero.
         '''
         return all(self[i] == 0 for i in range(order))
+
+    def almost_equals(self, other : 'Sequence', order=10):
+        r'''
+            Method that checks if two sequences are equals at the beginning.
+
+            This method receives another :class:`Sequence` and a number of elements to be checked and then it 
+            proceeds to check if all the first elements are equal for both sequences. This is helpful in some sequences
+            to guarantee equality through the whole sequence.
+
+            INPUT:
+
+            * ``sequence``: a :class:`Sequence` that will be compare with ``self``.
+            * ``order``: number of elements that will be compared.
+
+            OUTPUT:
+
+            ``True`` if the first ``order`` elements of both sequences are equal (even in different 
+            universes). ``False`` otherwise.
+
+            EXAMPLES::
+
+                sage: from pseries_basis.sequences import *
+                sage: def fib(n): return 1 if n == 0 else (1 if n==1 else fib(n-1) + fib(n-2))
+                sage: Fib = LambdaSequence(fib, ZZ)
+                sage: Fac = LambdaSequence(lambda n : factorial(n), ZZ)
+                sage: Fib[:10]
+                [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+                sage: Fac[:10]
+                [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+                sage: Fib.almost_equals(Fac, 2)
+                True
+                sage: Fib.almost_equals(Fac, 3)
+                True
+                sage: Fib.almost_equals(Fac, 4)
+                False
+        '''
+        return self[:order] == other[:order]
 
 class LambdaSequence(Sequence):
     r'''
@@ -75,6 +118,7 @@ class LambdaSequence(Sequence):
 
         * ``func``: a callable that defines the sequence as their output with natural input.
         * ``universe``: a parent structure where all the elements of the sequence will be (a specific casting is performed)
+        * ``allow_sym``: a flag to set a sequence to allow symbolic inputs to the sequence to obtain 'generic elements'.
 
         EXAMPLES::
 
@@ -90,16 +134,21 @@ class LambdaSequence(Sequence):
             sage: F[:10]
             [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
     '''
-    def __init__(self, func, universe):
+    def __init__(self, func, universe, allow_symb = False):
         super().__init__(universe)
 
         self.__func = func
+        self.__alls = allow_symb
 
     @cache
     def element(self, index : int):
+        output = self.__func(index)
         if self.universe is None: # we allow None universe
-            return self.__func(index)
-        return self.universe(self.__func(index))
+            return output
+        elif self.__alls and (not output in self.universe) and (output in SR):
+            return SR(output)
+        else:
+            return self.universe(output)
 
     # basic arithmethic methods
     def __add__(self, other):
@@ -138,4 +187,12 @@ class LambdaSequence(Sequence):
     def __rmul__(self, other):
         return self.__mul__(other)
 
+    # equality and hash methods
+    def __eq__(self, other):
+        if not isinstance(other, LambdaSequence):
+            return False
 
+        return self.__func == other.__func
+    
+    def __hash__(self):
+        return hash(self.__func)
