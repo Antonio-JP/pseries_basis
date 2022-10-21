e93332e47c74fc4a678db82f4a493231ce5bb5a1
Antonio Jimenez-Pastor <kethewal@gmail.com>
2022-10-21 13:16:19 +0200

    Added the import of noncom_rings into the all import of misc

--- pseries_basis/misc/__init__.py
+++ pseries_basis/misc/__init__.py
@@ -1,3 +1,11 @@
+r'''
+    Miscelaneous module.
+
+    This module contains all the necessary strucutres and algorithms to make things work in 
+    the main module :mod:`pseries_basis` that may be related with other structures of SageMath.
+'''
+
 from .io import *
+from .noncom_rings import *
 from .ore import *
 from .sequences import *
