aed0693911be7fdb6bc66e12c1e26908eb76b4ef
Antonio Jiménez-Pastor <kethewal@gmail.com>
2023-01-19 12:34:52 +0100

    Reviewed the file and added type hints

--- pseries_basis/factorial/gen_binomial_basis.py
+++ pseries_basis/factorial/gen_binomial_basis.py
@@ -7,14 +7,21 @@ try: # python 3.9 or higher
 except ImportError: #python 3.8 or lower
     from functools import lru_cache as cache
     
+from ore_algebra.ore_algebra import OreAlgebra_generic
+from ore_algebra.ore_operator import OreOperator
 from sage.all import ZZ, Matrix, vector, factorial, QQ
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.structure import element
+from typing import Collection
 
-from ..psbasis import PSBasis, check_compatibility
-from .factorial_basis import FallingBasis,BinomialBasis
+from ..psbasis import PSBasis, check_compatibility, Compatibility
+from .factorial_basis import FactorialBasis,FallingBasis,BinomialBasis
 from .product_basis import SievedBasis, ProductBasis
 
-def DefiniteSumSolutions(operator, *input):
+##################################################################################################################
+### DEFINITE SUM SOLUTIONS METHOD (see article)
+##################################################################################################################
+def DefiniteSumSolutions(operator: str | OreOperator | Compatibility, *input: int | list[int]) -> OreOperator:
     r'''
         Petkovšek's algorithm for transforming operators into recurrence equations.
         
@@ -103,8 +110,13 @@ def DefiniteSumSolutions(operator, *input):
     
     return result
 
+##################################################################################################################
+###
+### METHODS NECESSARY TO DEFINE THE GENERALIZED BINOMIAL BASIS
+###
+##################################################################################################################
 @cache
-def GeneralizedBinomial(a,b,c,m,r):
+def GeneralizedBinomial(a: int, b: int, c: int, m: int, r: int) -> FactorialBasis:
     r'''
         Method to get a basis which includes the general binomial coefficients.
         
@@ -181,92 +193,7 @@ def GeneralizedBinomial(a,b,c,m,r):
         
     return basis
 
-def unroll_sequence(operator, init, bound=10):
-    r'''
-        This method takes an operator of the shape
-
-        .. MATH::
-
-        p_d(n)S_n^d + \ldots p_1(n)S_n + p_0(n)
-
-        and some initial values given by ``init`` and unrolls
-        the sequence defined by that operator equal to zero
-        and the initial values up to some bound.
-    '''
-    d = operator.order()
-    if(d == 0):
-        if(all(el == 0 in init for el in init)):
-            return bound*[0]
-        raise ValueError("The initial values are not valid for the solution")
-
-    p = operator.coefficients()
-    solution = init[:d]
-    ## Checking the initial values
-    if(len(solution) < d):
-        raise ValueError("Not enough initial data was given")
-
-    for k in range(d, bound):
-        if(p[-1](n=k) == 0): # can not compute the value (we need it with ``init``)
-            if(k < len(init)):
-                solution += [init[k]]
-            raise ValueError("The initial data did not provided enough information (zero in leading coefficient at n=%d)" %k)
-        solution += [-sum(p[i](n=k)*solution[-d+i] for i in range(d))/p[-1](n=k)]
-        if(k < len(init)): # we check the value is correct
-            if(solution[-1] != init[k]):
-                raise ValueError("The initial values are not valid for the solution (too many given and do not fit at index %d)" %k)
-    
-    return solution
-
-def compute_in_basis(basis, sequence, section=1, shift=0, bound=10):
-    r'''
-        This method takes a :class:`pseries_basis.psbasis.PSBasis` and a sequence and computes
-        the expansion of that sequence in the usual power basis up to 
-        a bound.
-
-        If the sequence is given as a list, the bound may not be reached if not enough
-        data is provide.
-
-        We take only the elements in the basis that are in the `j`-th `F`-section
-        of the basis, i.e., elements of the shape `b_{kF+j}`. By default, the parameters
-        ``section`` and ``shift`` are prepared to consider the whole basis.
-    '''
-    ## Checking the section/shift arguments
-    if(not (section in ZZ and section > 0)):
-        raise ValueError("The section numbers must be a positive integer")
-    if(not (shift in ZZ and shift >= 0)):
-        raise ValueError("The shift must be a non-negative integer")
-    if(shift >= section):
-        raise ValueError("The shift must be smaller than the number of sections")
-
-    ## Checking the type of sequence
-    if(not (type(sequence) in (list, tuple))):
-        sequence = [sequence(i) for i in range(bound)]
-
-    ## Computing the iterative expansions
-    b = min(len(sequence), bound)
-    solution = [sequence[0]*basis[shift]]
-    for i in range(1,b):
-        solution += [solution[-1]+sequence[i]*basis[i*section+shift]]
-
-    return solution
-
-def unroll_in_basis(basis, operator, init, section=1, shift=0, bound=10):
-    r'''
-        This method takes a :class:`pseries_basis.psbasis.PSBasis` and a sequence in form of P-finite
-        recurrence (with initial data) and compute the expansion in the usual power 
-        basis up to a bound.
-
-        This method is equivalent to :func:`compute_in_basis`, but the sequence is 
-        not in list format or in a function but in a P-finite representation, that
-        can be unrolled using the method :func:`unroll_sequence`.
-
-        We take only the elements in the basis that are in the `j`-th `F`-section
-        of the basis, i.e., elements of the shape `b_{kF+j}`. By default, the parameters
-        ``section`` and ``shift`` are prepared to consider the whole basis.
-    '''
-    return compute_in_basis(basis, unroll_sequence(operator, init, bound), section, shift, bound)
-
-def multiset_inclusion(l1, l2):
+def multiset_inclusion(l1 : list|tuple, l2 : list|tuple) -> bool:
     r'''
         Method to check that the list ``l1`` is completely included in ``l2``.
 
@@ -285,7 +212,10 @@ def multiset_inclusion(l1, l2):
     '''
     return all(l1.count(el) <= l2.count(el) for el in set(l1))
 
-def guess_compatibility_E(basis, shift = 1, sections = None, A = None, bound_roots = 50, bound_data = 50):
+def guess_compatibility_E(basis: FactorialBasis, 
+    shift: element.Element = 1, sections: int = None, A: int = None, 
+    bound_roots: int = 50, bound_data: int = 50
+) -> OreOperator:
     r'''
         Method to guess the compatibility of a shift with a basis.
 
@@ -299,7 +229,7 @@ def guess_compatibility_E(basis, shift = 1, sections = None, A = None, bound_roo
         * ``shift``: value that is added to `x` with the shift we want to guess (i.e., `E(x) = x+\alpha` where
           `\alpha` is the value of ``shift``.
         * ``sections``: number of desired section in the compatibility condition.
-        * ``A``: vale for the compatibility bound `A`. If non is given, we guess a possible choice.
+        * ``A``: value for the compatibility bound `A`. If non is given, we guess a possible choice.
         * ``bound_roots``: bound for checking that the root characterization holds for ``basis``.
         * ``bound_data``: amount of data we compute i order to do the guessing.
 
@@ -351,7 +281,7 @@ def guess_compatibility_E(basis, shift = 1, sections = None, A = None, bound_roo
     ## returning the compatibility tuple
     return (A, 0, F, lambda i,j,k : functions[i][-j](n=F*k+j))
     
-def guess_rational_function(data, algebra):
+def guess_rational_function(data: Collection, algebra: OreAlgebra_generic) -> element.Element:
     # special case all zero
     if(all(el == 0 for el in data)):
         return algebra.base().zero()
